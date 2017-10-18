69dd8da0ca71a0ec61aa2eed2a25f42d1e5c51ae
Vector5233 <vector5233@gmail.com>
2017-10-18 15:58:33 -0400

    Add private motor test

--- TeamCode/src/main/java/DriveTrainOpMode.java
+++ TeamCode/src/main/java/DriveTrainOpMode.java
@@ -1,5 +1,7 @@
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by CCA on 10/18/2017.
@@ -8,6 +10,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 public class DriveTrainOpMode extends LinearOpMode {
     @Override
+    private DcMotor motorTest;
+    private Servo servoTest;
     public void runOpMode() throws InterruptedException {
 
     }
