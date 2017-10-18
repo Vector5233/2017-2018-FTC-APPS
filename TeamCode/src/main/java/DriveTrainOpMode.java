5fc142458d7a7c49aa8ed6f77c3a1def6908e162
Vector5233 <vector5233@gmail.com>
2017-10-18 15:54:26 -0400

    This is my second attempt at an opmode

---
+++ TeamCode/src/main/java/DriveTrainOpMode.java
@@ -0,0 +1,14 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by CCA on 10/18/2017.
+ */
+@TeleOp
+
+public class DriveTrainOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
