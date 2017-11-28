b40975c9ed8dce9c7d62aa04373b68de90bfd53e
Team 5233 Vector <vector5233@gmail.com>
2017-11-28 13:29:40 -0500

    Merge remote-tracking branches 'remotes/origin/Dev2' and 'remotes/origin/KF-Branch' into Dev1
    # Conflicts:
    #	TeamCode/src/main/java/DriveTrainOpMode.java
    #	TeamCode/src/main/java/MyFirstJavaClassOpMode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RaymondAutonomousOpMode.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode; /**
+ * Created by CCA on 10/26/2017.
+ */
+
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.Drive;
+
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
+
+
+@Autonomous(name="RaymondAutonomousOpMode", group = "myGroup")
+public class RaymondAutonomousOpMode extends Object {
+    Servo jewelKnocker, leftGrab, rightGrab = null;
+    DcMotor frontLeft, frontRight, backLeft, backRight = null;
+    ColorSensor colorSensor = null;
+    Drive drive;
+    LinearOpMode opmode;
+    float red, green, blue;
+    double JEWEL_UP = 0;
+    double JEWEL_DOWN = 0+0.091;
+
+    public RaymondAutonomousOpMode (Drive D, Servo JK, ColorSensor CS, LinearOpMode L) {
+        drive = D;
+        jewelKnocker = JK;
+        colorSensor = CS;
+        opmode = L;
+    }
+    public void JewelKnocker(){
+        jewelKnocker.setPosition(JEWEL_DOWN);
+        opmode.sleep(2000);
+        opmode.telemetry.addData("Red: ", colorSensor.red());
+        opmode.telemetry.addData("Blue: ", colorSensor.blue());
+        opmode.telemetry.update();
+        if (colorSensor.red() > colorSensor.blue()) {
+            drive.TurnRightDegree(0.3,21);
+            jewelKnocker.setPosition(JEWEL_UP);
+            drive.TurnLeftDegree(0.3,21);
+        } else {
+            drive.TurnLeftDegree(0.3,21);
+            jewelKnocker.setPosition(JEWEL_UP);
+            drive.TurnRightDegree(0.3,21);
+        }
+    }
+}
+
